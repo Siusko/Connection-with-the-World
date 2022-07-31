23bad0d3ebda180a00f59d6edea48146e51c50c4
siusko <4avyn1@gmail.com>
2022-07-31 10:58:04 +0300

    В операционной системе  Linux указывает путь к файлу csv и делает чтение его. После можно работать как с csv так и пребразовывать его в массив и интерполировать и фильтровать.

--- Approximation curve/data2curve.py
+++ Approximation curve/data2curve.py
@@ -1,6 +1,40 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-data= pd.read_csv("DefaultDataset.csv", delimiter=",")
-data[0]
+import plotly.express as px
 
-#plt.plot(data[0],R*cos(α))
+###Простой вызов файла
+data= pd.read_csv("DefaultDataset.csv", delimiter=",")
+data.columns = ["AAPL_x","AAPL_y"]
+fig = px.line(data, x = 'AAPL_x', y = 'AAPL_y', log_x=True) #по x log шкала
+fig.show()
+
+###Вызов указывающий путь к файлу
+data = pd.read_csv('/home/yesius/Documents/ФЛЕШКА/КОНФЕРЕНЦИИ И СТАТЬИ/2022/Ковтун/a.csv')
+data.columns = ["AAPL_x","AAPL_y"]
+fig = px.line(data, x = 'AAPL_x', y = 'AAPL_y') #по x log шкала
+fig.show()
+
+###открыть csv как массив
+import numpy as np
+with open("/home/yesius/Documents/ФЛЕШКА/КОНФЕРЕНЦИИ И СТАТЬИ/2022/Ковтун/a.csv") as file_name:
+    data = np.loadtxt(file_name, delimiter=",")
+#назначаем x и y
+x=data[:,0]
+y=data[:,1]
+len(x)
+from scipy.interpolate import interp1d
+f= interp1d(x, y)
+f2 = interp1d(x, y, kind='cubic')
+xnew = np.linspace(data[0,0], data[46,0], num=10000, endpoint=True)
+
+###Применение фильтра Савицкого-Голая к уже интерполированным данным
+import matplotlib.pyplot as plt
+
+plt.plot(xnew, f2(xnew))
+
+from scipy.signal import savgol_filter
+yhat = savgol_filter(f2(xnew),2000, 3) # window size 2000, polynomial order 3
+
+plt.plot(x,y)
+plt.plot(xnew,yhat, color='red')
+plt.show()
---
+++ Approximation curve/result equation.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import log,exp
+
+a=0.7326256560597258
+b=10.247099116246583
+y=9.6
+x=exp((y-b)/a)
+x
